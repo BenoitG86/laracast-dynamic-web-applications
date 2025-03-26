89114c451727e98170c5539ac22eb44ce38fb291
Benoît <benoit.grandval.muertes@univ-poitiers.fr>
2025-03-26 11:16:56 +0100

    style: modification page 404

--- views/404.php
+++ views/404.php
@@ -3,7 +3,10 @@
 
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1>Erreur 404 - page introuvable</h1>
+        <h1 class="text-2xl font-bold">Erreur 404 - page introuvable</h1>
+        <p class="mt-4">
+            <a href="/" class="text-blue underline">Go home, you're drunk.</a>
+        </p>
     </div>
 </main>
 
