821c09622486704a2e1b77bff6a264c17f125089
Benoît <benoit.grandval.muertes@univ-poitiers.fr>
2025-03-26 11:18:22 +0100

    refactor: deleting redundant require to functions.php

--- router.php
+++ router.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once 'functions.php';
-
 // On récupère en variable $uri l'adresse de la page actuelle avec $_SERVER['REQUEST_URI'], 
 // en récupérant que le path via parse_url()['path']
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
